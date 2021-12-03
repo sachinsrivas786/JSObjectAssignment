79f4246267fcd87e467e7dc0e639f2ef0047f98e
sachinsrivas786 <53460336+sachinsrivas786@users.noreply.github.com>
2021-12-04 02:08:19 +0530

    shape.js

---
+++ shape.js
@@ -0,0 +1,12 @@
+class Shape{
+ constructor(color){
+  this.color = color;
+ }
+ drawShape(){
+  console.log("Drawing Shape");
+ }
+ calculateArea(){
+  console.log("Calculating area of your shape")
+ }
+}
+module.exports = Shape;
